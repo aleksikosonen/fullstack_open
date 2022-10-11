202de2a3577b8c3770b82676716338acec5df34a
aleksikosonen <kososenaleksi@gmail.com>
2022-10-11 20:17:27 +0300

    osa2: tehtävä 2.6

--- osa2/src/App.js
+++ osa2/src/App.js
@@ -1,43 +1,4 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name}></Header>
-      <Content course={course}></Content>
-      <Total parts={course.parts}></Total>
-    </div>
-  )
-}
-
-const Header = ({ name }) => {
-  return <h2>{name}</h2>
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => {
-    return s + p.exercises
-  }, 0)
-  return <h3>Total of {total} exercises</h3>
-}
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  return course.parts.map((element) => {
-    return (
-      <Part
-        key={element.id}
-        part={element.name}
-        exercises={element.exercises}
-      />
-    )
-  })
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
