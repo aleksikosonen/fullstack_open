066bed50e64e16b12804390cb082dbed9b0dc67e
aleksikosonen <kososenaleksi@gmail.com>
2022-10-12 16:29:39 +0300

    osa2: tehtävä 2.6

--- osa2/src/App.js
+++ osa2/src/App.js
@@ -1,56 +1,37 @@
-import Course from "./components/Course"
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
+  }
 
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      {courses.map((course) => {
-        return <Course key={course.id} course={course} />
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return <div key={person.name}>{person.name}</div>
       })}
     </div>
   )
