c103406b5eb38a00ddb00ac8f7c2fa67f80a3510
aleksikosonen <kososenaleksi@gmail.com>
2022-10-15 16:40:01 +0300

    Osa2: tehtävä 2.13

--- osa2/src/components/Country.js
+++ osa2/src/components/Country.js
@@ -1,5 +1,26 @@
-const Country = ({country}) => {
-    return <div key={country.name.common}>{country.name.common}</div>
+import { useState } from 'react'
+import SingleCountry from './SingleCountry'
+
+const Country = ({ country }) => {
+  const [infoShown, setInfoShown] = useState(false)
+  return (
+    <div>
+      {infoShown ? (
+        <SingleCountry country={country} />
+      ) : (
+        <div key={country.name.common}>
+          {country.name.common}{' '}
+          <button
+            onClick={() => {
+              setInfoShown(!infoShown)
+            }}
+          >
+            show
+          </button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Country
