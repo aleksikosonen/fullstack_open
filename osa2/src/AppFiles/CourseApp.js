fc50bfb664351737aabb8f8d2d4f656942c87369
aleksikosonen <kososenaleksi@gmail.com>
2022-10-16 14:56:42 +0300

    Muokattu CourseApp.js tehtävän 2.5 mukaiseksi

--- osa2/src/AppFiles/CourseApp.js
+++ osa2/src/AppFiles/CourseApp.js
@@ -1,45 +1,8 @@
 /*
 Kopioimalla tämän App.js tiedostoon saa 2.5 kurssitiedot-tehtävän vastaukset toimimaan.
- */const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name}></Header>
-      <Content course={course}></Content>
-      <Total parts={course.parts}></Total>
-    </div>
-  )
-}
+ */
 
-const Header = ({ name }) => {
-  return <h2>{name}</h2>
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, p) => {
-    return s + p.exercises
-  }, 0)
-  return <h3>Total of {total} exercises</h3>
-}
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  return course.parts.map((element) => {
-    return (
-      <Part
-        key={element.id}
-        part={element.name}
-        exercises={element.exercises}
-      />
-    )
-  })
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
