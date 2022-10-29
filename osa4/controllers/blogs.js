18191ff831f7ac2b0e3ef58ecd590ef30959df67
aleksikosonen <kososenaleksi@gmail.com>
2022-10-29 09:15:58 +0300

    Osa4: tehtävä 4.13 + muuta parantelua

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,18 +1,20 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blog = await Blog.find({})
+  response.json(blog)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some notes saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 /* //at the time of initial testing there are two entities in mongodb
@@ -18,26 +33,42 @@ test('there are two notes', async () => {
   expect(response.body).toHaveLength(2)
 })*/
 
-test('blogs can be added', async () => {
-  const initialBlogs = await api.get('/api/blogs')
+describe('addition of a new test', () => {
+  test('blogs can be added', async () => {
+    const initialBlogs = await api.get('/api/blogs')
 
-  const blog = {
-    title: 'Gravelpyöräily',
-    author: 'Koistine',
-    url: 'www',
-    likes: '100',
-  }
+    const blog = {
+      title: 'Gravelpyöräily',
+      author: 'Koistine',
+      url: 'www',
+      likes: '100',
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(blog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(blog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const blogsWithAddedBlog = await api.get('/api/blogs')
-  const rightContent = blogsWithAddedBlog.body.map((blog) => blog.title)
-  expect(blogsWithAddedBlog.body).toHaveLength(initialBlogs.body.length + 1)
-  expect(rightContent).toContain('Gravelpyöräily')
+    const blogsWithAddedBlog = await api.get('/api/blogs')
+    const rightContent = blogsWithAddedBlog.body.map((blog) => blog.title)
+    expect(blogsWithAddedBlog.body).toHaveLength(initialBlogs.body.length + 1)
+    expect(rightContent).toContain('Gravelpyöräily')
+  })
+})
+
+describe('delete blog', () => {
+  test('a blog can be deleted', async () => {
+    const initialBlogs = await api.get('/api/blogs')
+    const blogToDelete = initialBlogs.body[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+    const blogsAtEnd = await api.get('/api/blogs')
+    expect(blogsAtEnd.body).toHaveLength(initialBlogs.body.length - 1)
+
+    const contents = blogsAtEnd.body.map((blog) => blog.title)
+    expect(contents).not.toContain(blogToDelete.title)
+  })
 })
 
 afterAll(() => {
