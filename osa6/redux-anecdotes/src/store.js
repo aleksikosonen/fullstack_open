25b97dea48710b6a8842bc47bfdad69eab872937
aleksikosonen <kososenaleksi@gmail.com>
2022-11-06 14:10:00 +0200

    Osa6: tehtävät 6.10 lisätty combinedReducers

--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
+import { combineReducers } from 'redux'
 import notificationReducer from './reducers/notificationReducer'
 
-const store = configureStore({
-  reducer: {
+const reducer = combineReducers({
     anecdotes: anecdoteReducer,
     notification: notificationReducer
-  },
+  })
+
+const store = configureStore({
+  reducer: reducer
 })
 
 export default store
