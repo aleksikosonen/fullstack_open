689ca3d10971e8e2d25e16f9aae000609bf4792f
aleksikosonen <kososenaleksi@gmail.com>
2022-12-01 18:08:09 +0200

    Part 9: exercise 9.3

--- osa9/bmiCalculator.ts
+++ osa9/bmiCalculator.ts
@@ -1,26 +1,43 @@
 const bmiCalculator = (height: number, weight: number) => {
-    /*The formula is BMI = kg/m2 where kg is a person's weight in kilograms and m2 is their height in metres squared */
-    const bmiNumber: number = weight / ((height / 100) ** 2)
+  /*The formula is BMI = kg/m2 where kg is a person's weight in kilograms and m2 is their height in metres squared */
+  const bmiNumber: number = weight / (height / 100) ** 2
 
-    if (bmiNumber < 16) return 'Underweight (Severe thinness)'
-    if (bmiNumber < 16.9) return 'Underweight (Moderate thinness)'
-    if (bmiNumber < 18.4) return 'Underweight (Mild thinness)'
-    if (bmiNumber < 24.9) return 'Normal range'
-    if (bmiNumber < 29.9) return 'Overweight (Pre-obese)'
-    if (bmiNumber < 34.9) return 'Obese (Class I)'
-    if (bmiNumber < 39.9) return 'Obese (Class II)'
-    if (bmiNumber > 40) return 'Obese (Class III)'
+  if (bmiNumber < 16) return 'Underweight (Severe thinness)'
+  if (bmiNumber < 16.9) return 'Underweight (Moderate thinness)'
+  if (bmiNumber < 18.4) return 'Underweight (Mild thinness)'
+  if (bmiNumber < 24.9) return 'Normal range'
+  if (bmiNumber < 29.9) return 'Overweight (Pre-obese)'
+  if (bmiNumber < 34.9) return 'Obese (Class I)'
+  if (bmiNumber < 39.9) return 'Obese (Class II)'
+  if (bmiNumber > 40) return 'Obese (Class III)'
+}
+
+interface BmiValues {
+  height: number
+  weight: number
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
   }
+}
 
-  console.log(bmiCalculator(180, 46))
-  console.log(bmiCalculator(180, 54))
-  console.log(bmiCalculator(180, 56))
-  console.log(bmiCalculator(180, 60))
-  console.log(bmiCalculator(180, 66))
-  console.log(bmiCalculator(180, 76))
-  console.log(bmiCalculator(180, 86))
-  console.log(bmiCalculator(180, 100))
-  console.log(bmiCalculator(180, 106))
-  console.log(bmiCalculator(180, 116))
-  console.log(bmiCalculator(180, 126))
-  console.log(bmiCalculator(180, 136))
+try {
+  const { height, weight } = parseArguments(process.argv)
+  console.log(bmiCalculator(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
--- osa9/exerciseCalculator.ts
+++ osa9/exerciseCalculator.ts
@@ -1,19 +1,23 @@
 interface Result {
-    periodLength: number,
-    trainingDays: number,
-    success: boolean,
-    rating: number,
-    ratingDescription: string,
-    target: number,
-    average: number 
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
 }
 
 const calculateExercises = (trainingWeek: number[], target: number): Result => {
-    const periodLength = trainingWeek.length
-    const trainingDays = trainingWeek.filter(dailyHours => dailyHours > 0).length
-    const trainingHours = trainingWeek.reduce((dayTotal, hours) => dayTotal + hours )
+  const periodLength = trainingWeek.length
+  const trainingDays = trainingWeek.filter(
+    (dailyHours) => dailyHours > 0
+  ).length
+  const trainingHours = trainingWeek.reduce(
+    (dayTotal, hours) => dayTotal + hours
+  )
 
-    /* Initially went with this as this would be the recommendation
+  /* Initially went with this as this would be the recommendation
     fixed to suit the exercise model answer, where target is target hours
 
     // The department recommends training 150 minutes of moderate aerobic activity per week
@@ -21,36 +25,72 @@ const calculateExercises = (trainingWeek: number[], target: number): Result => {
     const dailyAverage = trainingMinutes / periodLength
     const suggestedDailyAverage = 150 / 7 */
 
+  let rating = 0
+  let ratingDescription = 'initial'
+  console.log(trainingHours)
+  const highHours = 14
+  const mediumHours = 7
 
-    let rating = 0
-    let ratingDescription = 'initial'
-    console.log(trainingHours)
-    const highHours = 14
-    const mediumHours = 7
+  if (trainingHours > highHours) {
+    rating = 3
+    ratingDescription = 'great'
+  } else if (trainingHours > mediumHours) {
+    rating = 2
+    ratingDescription = 'not too bad but could be better'
+  } else {
+    rating = 1
+    ratingDescription = 'not too great'
+  }
 
-    if (trainingHours > highHours) {
-        rating = 3
-        ratingDescription = 'great'
-    } else if (trainingHours > mediumHours) {
-        rating = 2
-        ratingDescription = 'not too bad but could be better'
-    } else  {
-        rating = 1
-        ratingDescription = 'not too great'
-    }
+  const average = trainingHours / periodLength
+  const success = average >= target
 
-    const average = trainingHours / periodLength
-    const success = average >= target
-
-    return {
-        periodLength,
-        trainingDays,
-        success,
-        rating,
-        ratingDescription,
-        target,
-        average 
-    }
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+interface ExerciseValues {
+  target: number
+  hours: number[]
+}
+
+const parseArgumentsExercise = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+
+  const target = Number(args[2])
+  const hoursInitial = args.slice(3)
+  const hours = hoursInitial.map((hour) => Number(hour))
+
+  if (isNaN(Number(target))) {
+    throw new Error('First value needs to be number')
+  }
+
+  hours.map((hour) => {
+    if (isNaN(Number(hour))) {
+      throw new Error('Please add only numbers')
+    }
+  })
+
+  return {
+    target,
+    hours,
+  }
+}
+
+try {
+  const { target, hours } = parseArgumentsExercise(process.argv)
+  console.log(calculateExercises(hours, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
