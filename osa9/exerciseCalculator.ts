8aa024a94f4bde0e202035fdff7b04e011830dfd
aleksikosonen <kososenaleksi@gmail.com>
2022-12-01 16:33:37 +0200

    Part 9: Exercise 9.2

--- osa9/exerciseCalculator.ts
+++ osa9/exerciseCalculator.ts
@@ -9,12 +9,38 @@ interface Result {
 }
 
 const calculateExercises = (trainingWeek: number[], target: number): Result => {
-    const periodLength = 0
-    const trainingDays = 0
-    const success = true
-    const rating = 0
-    const ratingDescription = 'initial'
-    const average = 0 
+    const periodLength = trainingWeek.length
+    const trainingDays = trainingWeek.filter(dailyHours => dailyHours > 0).length
+    const trainingHours = trainingWeek.reduce((dayTotal, hours) => dayTotal + hours )
+
+    /* Initially went with this as this would be the recommendation
+    fixed to suit the exercise model answer, where target is target hours
+
+    // The department recommends training 150 minutes of moderate aerobic activity per week
+    const trainingMinutes = trainingHours * 60
+    const dailyAverage = trainingMinutes / periodLength
+    const suggestedDailyAverage = 150 / 7 */
+
+
+    let rating = 0
+    let ratingDescription = 'initial'
+    console.log(trainingHours)
+    const highHours = 14
+    const mediumHours = 7
+
+    if (trainingHours > highHours) {
+        rating = 3
+        ratingDescription = 'great'
+    } else if (trainingHours > mediumHours) {
+        rating = 2
+        ratingDescription = 'not too bad but could be better'
+    } else  {
+        rating = 1
+        ratingDescription = 'not too great'
+    }
+
+    const average = trainingHours / periodLength
+    const success = average >= target
 
     return {
         periodLength,
