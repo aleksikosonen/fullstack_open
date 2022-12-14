94a6f8130de15de0d51f3d1f44dac7089a9eb128
aleksikosonen <kososenaleksi@gmail.com>
2022-12-14 17:06:29 +0200

    Part 9: task 9.16-18

--- osa9/patientor/frontend/src/PatientPage/index.tsx
+++ osa9/patientor/frontend/src/PatientPage/index.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { Box, Typography } from '@material-ui/core'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { apiBaseUrl } from '../constants'
+import { useStateValue, updatePatientList } from '../state'
+import { Gender, Patient } from '../types'
+import './PatientPage.css'
+import MaleIcon from '@mui/icons-material/Male';
+import TransgenderIcon from '@mui/icons-material/Transgender';
+import FemaleIcon from '@mui/icons-material/Female';
 
 const PatientPage = () => {
-    return (<div>moro</div>)
+  const { id } = useParams<{ id: string }>()
+  const [{ patients }, dispatch] = useStateValue()
+  const [patientById, setPatientById] = useState<Patient>()
+
+  const fetchPatient = async () => {
+    // to make sure patient is not re-fetched
+    console.log('fetchPatient')
+    try {
+      const { data: patient } = await axios.get<Patient>(
+        `${apiBaseUrl}/patients/${id}`
+      )
+      setPatientById(patient)
+      dispatch(updatePatientList(patient))
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e)) {
+        console.error(e?.response?.data || 'Unrecognized axios error')
+      } else {
+        console.error('Unknown error', e)
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (id === undefined) {
+      throw new Error('id undefined')
+    }
+    if (patients[id].ssn) {
+      setPatientById(patients[id])
+    } else {
+      fetchPatient()
+    }
+  }, [dispatch])
+
+  const getIconForGender = (gender: Gender) => {
+    if (gender === Gender.Male) {
+        return <MaleIcon/>
+    }
+    if (gender === Gender.Female) {
+        return <FemaleIcon/>
+    }
+    if (gender === Gender.Other) {
+        return <TransgenderIcon/>
+    }
+  }
+
+  return (
+    <div className='patient'>
+      <div className='bold'>{patientById?.name} {getIconForGender(patientById?.gender as Gender)}</div>
+      <div>ssh: {patientById?.ssn}</div>
+      <div>occupation: {patientById?.occupation}</div>
+    </div>
+  )
 }
 
 export default PatientPage
--- osa9/patientor/frontend/src/state/reducer.ts
+++ osa9/patientor/frontend/src/state/reducer.ts
@@ -1,19 +1,44 @@
-import { State } from "./state";
-import { Patient } from "../types";
+import { State } from './state'
+import { Patient } from '../types'
 
 export type Action =
   | {
-      type: "SET_PATIENT_LIST";
-      payload: Patient[];
+      type: 'SET_PATIENT_LIST'
+      payload: Patient[]
     }
   | {
-      type: "ADD_PATIENT";
-      payload: Patient;
-    };
+      type: 'UPDATE_PATIENT'
+      payload: Patient
+    }
+  | {
+      type: 'ADD_PATIENT'
+      payload: Patient
+    }
+
+export const setPatientList = (patientListFromApi: Patient[]): Action => {
+  return {
+    type: 'SET_PATIENT_LIST',
+    payload: patientListFromApi
+  }
+}
+
+export const addPatient = (addedPatient: Patient): Action => {
+  return {
+    type: 'ADD_PATIENT',
+    payload: addedPatient
+  }
+}
+
+export const updatePatientList = (patientFromApi: Patient): Action => {
+  return {
+    type: 'UPDATE_PATIENT',
+    payload: patientFromApi
+  }
+}
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_PATIENT_LIST":
+    case 'SET_PATIENT_LIST':
       return {
         ...state,
         patients: {
@@ -21,18 +46,26 @@ export const reducer = (state: State, action: Action): State => {
             (memo, patient) => ({ ...memo, [patient.id]: patient }),
             {}
           ),
-          ...state.patients
-        }
-      };
-    case "ADD_PATIENT":
+          ...state.patients,
+        },
+      }
+    case 'UPDATE_PATIENT':
       return {
         ...state,
         patients: {
           ...state.patients,
-          [action.payload.id]: action.payload
-        }
-      };
+          [action.payload.id]: action.payload,
+        },
+      }
+    case 'ADD_PATIENT':
+      return {
+        ...state,
+        patients: {
+          ...state.patients,
+          [action.payload.id]: action.payload,
+        },
+      }
     default:
-      return state;
+      return state
   }
-};
+}
