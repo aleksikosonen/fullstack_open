9d6fd79af8bbd69bb043a3ed83c765389421b455
aleksikosonen <kososenaleksi@gmail.com>
2023-01-03 16:13:39 +0200

    Osa4: tehtävät 4.5-7

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -7,7 +7,53 @@ const totalLikes = (blogs) => {
   return likes
 }
 
+const favoriteBlog = (blogs) => {
+  const mostLikedBlog = blogs.reduce((mostLikes, currentLikes) =>
+    mostLikes.likes > currentLikes.likes ? mostLikes : currentLikes
+  )
+  return mostLikedBlog
+}
+
+const mostBlogs = (blogs) => {
+  const authorWithMostBlogs = blogs.reduce((mostBlogs, currentBlog) => {
+    mostBlogs[currentBlog.author] = (mostBlogs[currentBlog.author] || 0) + 1
+    return currentBlog
+  })
+  console.log(authorWithMostBlogs)
+  const blogsThatHaveAuthor = blogs.filter(
+    (blog) => blog.author === authorWithMostBlogs.author
+  )
+  const blogLikesSum = blogsThatHaveAuthor.length
+  const mostBlogs = { author: authorWithMostBlogs.author, blogs: blogLikesSum }
+  return mostBlogs
+}
+
+const mostLikes = (blogs) => {
+  const authors = []
+
+  blogs.filter((blog) => {
+    if (!authors.includes(blog.author)) {
+      authors.push({author: blog.author, likes: 0})
+    }
+  })
+
+  authors.forEach((author) => {
+    blogs.forEach((blog) => {
+      if (blog.author === author.author)
+      author.likes += blog.likes
+      authors.sort((a,b)=> {return (b.likes - a.likes)})
+    })
+  })
+
+  const authorToReturn = authors[0]
+
+  return authorToReturn
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
